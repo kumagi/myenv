aa1508da2e4cedb0ba630464cd605bad0e037e39
Hiroki Kumzaki <hiroki.kumazaki@gmail.com>
2012-02-15 14:06:31 +0900

    changed

---
+++ .zsh.d/completion.zsh
@@ -0,0 +1,86 @@
+
+# 補完
+## 初期化
+autoload -U compinit
+compinit
+
+## 補完方法毎にグループ化する。
+### 補完方法の表示方法
+###   %B...%b: 「...」を太字にする。
+###   %d: 補完方法のラベル
+zstyle ':completion:*' format '%F{white}%B%d%b%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-colors 'di=1;34'
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' keep-prefix
+zstyle ':completion:*' remote-access false
+zstyle ':completion:*' completer _oldlist _complete _match _ignored \
+    _list _history
+zstyle ':completion:sudo:*' environ PATH="$SUDO_PATH:$PATH"
+## 補完侯補をメニューから選択する。
+### select=2: 補完候補を一覧から選択する。
+###           ただし、補完候補が2つ以上なければすぐに補完する。
+zstyle ':completion:*:default' menu select=2
+
+## 補完候補に色を付ける。
+### "": 空文字列はデフォルト値を使うという意味。
+zstyle ':completion:*:default' list-colors ""
+
+## 補完候補がなければより曖昧に候補を探す。
+### m:{a-z}={A-Z}: 小文字を大文字に変えたものでも補完する。
+### r:|[._-]=*: 「.」「_」「-」の前にワイルドカード「*」があるものとして補完する。
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z} r:|[._-]=*'
+
+## 補完方法の設定。指定した順番に実行する。
+### _oldlist 前回の補完結果を再利用する。
+### _complete: 補完する。
+### _match: globを展開しないで候補の一覧から補完する。
+### _history: ヒストリのコマンドも補完候補とする。
+### _ignored: 補完候補にださないと指定したものも補完候補とする。
+### _approximate: 似ている補完候補も補完候補とする。
+### _prefix: カーソル以降を無視してカーソル位置までで補完する。
+zstyle ':completion:*' completer \
+    _oldlist _complete _match _history _ignored _approximate _prefix
+
+## 補完候補をキャッシュする。
+zstyle ':completion:*' use-cache yes
+## 詳細な情報を使う。
+#zstyle ':completion:*' verbose yes
+## sudo時にはsudo用のパスも使う。
+zstyle ':completion:sudo:*' environ PATH="$SUDO_PATH:$PATH"
+
+## カーソル位置で補完する。
+setopt complete_in_word
+## globを展開しないで候補の一覧から補完する。
+setopt glob_complete
+## 補完時にヒストリを自動的に展開する。
+setopt hist_expand
+## 補完候補がないときなどにビープ音を鳴らさない。
+setopt no_beep
+## 辞書順ではなく数字順に並べる。
+setopt numeric_glob_sort
+
+
+# auto-fu
+package-install github hchbaw/auto-fu.zsh
+
+source $(package-directory hchbaw/auto-fu.zsh)/auto-fu.zsh
+
+
+zstyle ':auto-fu:highlight' input bold
+zstyle ':auto-fu:highlight' completion fg=white,dim
+zstyle ':auto-fu:highlight' completion/one fg=blue,dim
+zstyle ':auto-fu:var' postdisplay ''
+zstyle ':auto-fu:var' track-keymap-skip opp
+
+zle-line-init() {
+    auto-fu-init
+}
+zle -N zle-line-init
+
+zle -N zle-keymap-select auto-fu-zle-keymap-select
+
+afu+cancel-and-accept-line() {
+    ((afu_in_p == 1)) && { afu_in_p=0; BUFFER="$buffer_cur" }
+    zle afu+accept-line
+}
